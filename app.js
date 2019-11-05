938276b961a5d3177dd38ecac82bcb5afe587a17
Yahia Refaiea yahiarefaiea@gmail.com <yahiarefaiea@gmail.com>
2019-11-05 03:29:49 +0200

    add Morgan for cli logging

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = new express()
 
+app.use(morgan('dev'))
 app.use(express.json())
 
 app.use('/api/item', require('./demo/routes'))
