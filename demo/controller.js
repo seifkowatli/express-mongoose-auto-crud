c62f2f49f6d286b5f124aa213bec666ff2f5f5e7
Yahia Refaiea yahiarefaiea@gmail.com <yahiarefaiea@gmail.com>
2019-11-05 04:18:28 +0200

    refactore controller and abstractController

--- demo/controller.js
+++ demo/controller.js
@@ -1,9 +1,10 @@
 import AbstractController from '../lib/abstractController'
+import Model from './model'
 
 class Ctrl extends AbstractController {
-  constructor(model, options) {
-    super(model, options)
+  constructor(model, modelName, options) {
+    super(model, modelName, options)
   }
 }
 
-module.exports = new Ctrl('Item')
+module.exports = new Ctrl(Model, 'Item')
