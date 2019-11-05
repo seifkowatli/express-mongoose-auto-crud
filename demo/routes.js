1235edef4e0a7944c3255225b6bae1c84aaf8736
Yahia Refaiea yahiarefaiea@gmail.com <yahiarefaiea@gmail.com>
2019-11-05 04:40:52 +0200

    add a working example of a class that works!!

--- demo/routes.js
+++ demo/routes.js
@@ -4,4 +4,20 @@ const router = express.Router()
 
 router.post('/', Ctrl.create)
 
+
+// -- test starts
+import Task from '../lib/task'
+
+var task1 = new Task('create a demo for constructors');
+var task2 = new Task('create a demo for modules');
+var task3 = new Task('create a demo for singletons');
+var task4 = new Task('create a demo for prototypes');
+
+task1.complete();
+task2.save();
+task3.save();
+task4.save();
+// -- test ends
+
+
 module.exports = router
